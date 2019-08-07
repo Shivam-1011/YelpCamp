9b2d9bc2cbd199953015e117203b9844372f6ecb
Shivam <aggarwalshivam6@gmail.com>
2019-08-07 12:51:55 +0000

    deploying to heroku

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(function(req, res, next){
 	next();
 });
 
+//Some changes for git commit
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
